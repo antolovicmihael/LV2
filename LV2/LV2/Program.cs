3d08b3fd717745523e4ed22799caa376c44d86f7
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-04-30 16:45:11 +0200

    1 zadatak

---
+++ LV2/LV2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LV2
+{
+    class Die
+    {
+        private int sidesCount;
+        private Random random;
+        public Die(int sidesCount)
+        {
+            this.sidesCount= sidesCount;
+            random = new Random();
+        }
+        public int Roll(int sidesCount)
+        {
+            return random.Next(1, sidesCount + 1);
+        }
+        static void Main(string[] args)
+        {
+            DiceRoller roller = new DiceRoller();
+            Die dice = new Die(6);
+            for (int i = 0; i < 20; i++)
+            {
+                roller.InsertDie(dice);
+                dice.Roll(6);
+                Console.Write(roller);
+            }
+        }
+    }
+}
